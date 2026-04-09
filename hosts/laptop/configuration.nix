efe46b31a1386e4f67212116c59a9692b24e9ac8
Stanisław Dera <stanislaw@dera.dev>
2026-04-09 21:30:56 +0200

    add drivers fix for laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -14,5 +14,32 @@
     powertop
   ];
 
+  # Input device lag fix
+  boot.extraModprobeConfig = ''
+    options usbcore autosuspend=-1
+  '';
+
+  # Intel Iris Xe and dual monitor fix
+  boot.kernelParams = [
+    "i915.enable_guc=3"
+    "i915.enable_fbc=1"
+  ];
+
+  # inotify for Next.js
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_watches" = 524288;
+  };
+
+  # Hardware acceleration
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      libva-vdpau-driver
+      libvdpau-va-gl
+    ];
+  };
+
   system.stateVersion = "25.11";
 }
